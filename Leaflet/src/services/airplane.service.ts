0cae0b128d69623d68182d1c08e8b8e2cfeb8657
Snow-kal <1820661379@qq.com>
2025-11-11 19:52:13 +0800

    fix: 弹窗居中

--- Leaflet/src/services/airplane.service.ts
+++ Leaflet/src/services/airplane.service.ts
@@ -1,13 +1,13 @@
-import apiClient from './apiClient';
+import apiClient from "./apiClient";
 
 export const publishAirplane = (content: string) => {
-  return apiClient.post('/api/airplane', { content });
+    return apiClient.post("/api/airplane", { content });
 };
 
 export const pickAirplane = () => {
-  return apiClient.get('/api/airplane');
+    return apiClient.get("/api/airplane");
 };
 
 export const getDailyLimit = () => {
-  return apiClient.get('/api/airplane/daily-limit');
-};   
+    return apiClient.get("/api/airplane/daily-limit");
+};
